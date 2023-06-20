03fb8b9943f57be1af818cef18e65f3e37e435a1
ComusBala <comusbala96@gmail.com>
2023-06-20 16:15:55 +0530

    The final news app

---
+++ react-news-app/src/react-news-app/pages/profile/personalize/History.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Profilebar from "../../../components/profile/Profilebar";
+import CurrentNewsPost from "../../../components/newsPosts/currentNews/CurrentNewsPost";
+export default function History() {
+  return (
+    <>
+      <Profilebar />
+      <CurrentNewsPost />
+    </>
+  );
+}
